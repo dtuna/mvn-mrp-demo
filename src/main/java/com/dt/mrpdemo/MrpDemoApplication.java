45ae24da36780505d33cf25dc3e1aeee182db6e8
dtuna <doga.tuna@gmail.com>
2021-01-10 22:36:33 +0300

    initial project

---
+++ src/main/java/com/dt/mrpdemo/MrpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dt.mrpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrpDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dt/mrpdemo/MrpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dt.mrpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrpDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
